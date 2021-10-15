9919fa4a29bd616813de5df803eec56f4774feeb
Noname400 <74019756+Noname400@users.noreply.github.com>
2021-10-15 22:48:52 +0500

    Update addr_to_h160.py

--- BF-create/addr_to_h160.py
+++ BF-create/addr_to_h160.py
@@ -4,37 +4,36 @@ import sys
 import base58
 import codecs
 
-
-def count_lines(file):
-	return sum(1 for line in open(file, 'r'))
-
-
-def convert(file_in,file_out,nom):
+def convert(file_in,file_out):
     print("===========File input -> " + file_in)
     print("===========File output -> " + file_out)
     i = 0
     line_10 = 100000
     ii = 0
+    count = 0
     f = open(file_in,'r')
     fw = open(file_out,'a')
-    while i < nom:
-        if (i+ii) == nom:
-            print("\n Finish")
-            break
-        if line_10 == i:
-            print("Error - {} | Total line -> {}".format(ii,line_10),end='\r')
-            line_10 += 100000
+    while True:
+        adr58 = f.readline().strip()
+        if not adr58:
+            print('Finish!')
+            f.close()
+            fw.close()
+            sys.exit()
+        count += 1
+
+        if count == line_10:
+            print("skip: {} | pass line: {} | total: {}".format(ii,i,count),end='\r')
+            line_10 +=10000
+
         try:
-            adr58 = f.readline().strip()
             adr160 = base58.b58decode_check(adr58).hex()[2:]
         except:
             ii +=1
         else:
-            #fw.write(hash160+'\n')
             fw.write(adr160+'\n')
             i += 1
-    f.close()
-    fw.close()
+
 
 
 if __name__ == "__main__":
@@ -50,6 +49,4 @@ if __name__ == "__main__":
     file_in = sys.argv[1]
     file_out = sys.argv[2]
 
-    line_count = count_lines(file_in)
-    print("all lines -> " + str(line_count))
-    convert(file_in,file_out,line_count)
+    convert(file_in,file_out)
